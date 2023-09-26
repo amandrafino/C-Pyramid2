4caa5c0cdf828a2d8d1f62d90ded1090aec52969
amandrafino <amandrafino@gmail.com>
2023-09-26 12:10:39 -0400

    updated file names

--- cp2.v05.c
+++ cp2.v05.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Get size of grid
 int getGridSize() {
     int n;
-    do {
-        printf("Please enter a number (1 - 10): ");
+    do{ 
+        printf("Enter a number [1 - 10]: ");
         scanf("%d", &n);
     } while (n < 1 || n > 10);
 
-    return n; // Returns  the value n, which is the  size of the grid
+    return n; // Returns value of n (size of grid)
 }
 
-// Print Grid Pattern
-void printGrid(int n) {
+// Pring Grid Pattern
+void printGridPattern(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("#");
@@ -24,6 +25,6 @@ void printGrid(int n) {
 // Call the functions
 int main() {
     int gridSize = getGridSize();
-    printGrid(gridSize);
+    printGridPattern(gridSize);
     return 0;
 }
